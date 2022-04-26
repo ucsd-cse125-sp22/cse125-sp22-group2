5be073279878d621339e75d8bd5eb3c970dae5e0
sjm014 <sjm014@ucsd.edu>
2022-04-25 19:55:54 -0700

    Fixed left and right movement

--- server/Server.cpp
+++ server/Server.cpp
@@ -37,13 +37,13 @@ void gameLoop(PhysicalObjectManager* manager, int clientID, cse125framing::Movem
     PhysicalObject* player = manager->objects->at(clientID);
     switch (movementKey) {
     case cse125framing::MovementKey::RIGHT:
-        player->moveDirection(glm::normalize(vec3(-cameraDirection.x, cameraDirection.y, cameraDirection.z)));
+        player->moveDirection(glm::normalize(vec3(-cameraDirection.z, cameraDirection.y, cameraDirection.x)));
         break;
     case cse125framing::MovementKey::FORWARD:
         player->moveDirection(glm::normalize(cameraDirection));
         break;
     case cse125framing::MovementKey::LEFT:
-        player->moveDirection(glm::normalize(vec3(cameraDirection.x, cameraDirection.y, -cameraDirection.z)));
+        player->moveDirection(glm::normalize(vec3(cameraDirection.z, cameraDirection.y, -cameraDirection.x)));
         break;
     case cse125framing::MovementKey::BACKWARD:
         player->moveDirection(glm::normalize(-cameraDirection));
