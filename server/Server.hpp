6c1e172dc7712b9803e89161728dd8bb2f14e532
Evan Serrano <evanjserrano@gmail.com>
2022-05-11 11:25:39 -0700

    format

--- server/Server.hpp
+++ server/Server.hpp
@@ -1,35 +1,39 @@
 #pragma once
 
-#include <deque>
 #include <boost/asio.hpp>
+#include <deque>
 #include <mutex>
 
 #include "../Frame.hpp"
 
 class GraphicsSession : public std::enable_shared_from_this<GraphicsSession>
 {
-public:
+  public:
     bool sessionTerminated;
-    GraphicsSession(boost::asio::ip::tcp::socket socket, int id, std::deque<cse125framing::ClientFrame>& serverQueue, std::mutex& queueMtx, unsigned int& clientsConnected);
+    GraphicsSession(boost::asio::ip::tcp::socket socket,
+                    int id,
+                    std::deque<cse125framing::ClientFrame>& serverQueue,
+                    std::mutex& queueMtx,
+                    unsigned int& clientsConnected);
 
     void start();
 
     void do_read();
     void do_write(cse125framing::ServerFrame* serverFrame);
 
-private:
+  private:
     boost::asio::ip::tcp::socket socket;
     int id;
     unsigned int& clientsConnected;
     std::deque<cse125framing::ClientFrame>& serverQueue;
     std::mutex& queueMtx;
-	boost::array<char, cse125framing::CLIENT_FRAME_BUFFER_SIZE> clientBuffer;
-	boost::array<char, cse125framing::SERVER_FRAME_BUFFER_SIZE> serverBuffer;
+    boost::array<char, cse125framing::CLIENT_FRAME_BUFFER_SIZE> clientBuffer;
+    boost::array<char, cse125framing::SERVER_FRAME_BUFFER_SIZE> serverBuffer;
 };
 
 class GraphicsServer
 {
-public:
+  public:
     std::deque<cse125framing::ClientFrame> serverQueue;
     std::mutex queueMtx;
     unsigned int clientsConnected;
@@ -37,7 +41,7 @@ public:
     GraphicsServer(boost::asio::io_context& io_context, short port);
     void writePackets(cse125framing::ServerFrame* serverFrame);
 
-private:
+  private:
     void do_accept();
 
     std::vector<std::shared_ptr<GraphicsSession>> sessions;
