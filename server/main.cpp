95397b11d46bd60830071609077332da609f6604
Evan Serrano <evanjserrano@gmail.com>
2022-05-10 23:09:27 -0700

    basic format and cleanup

--- server/main.cpp
+++ server/main.cpp
@@ -1,19 +1,19 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <unordered_set>
-#include <unordered_map>
 #include <boost/array.hpp>
 #include <boost/asio.hpp>
 #include <boost/thread.hpp>
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
 
 #include "../Config.hpp"
 #include "../Constants.hpp"
 #include "../Frame.hpp"
 #include "../GameLogic/PhysicalObjectManager.hpp"
+#include "ClockTick.hpp"
 #include "GameAction.hpp"
 #include "Server.hpp"
-#include "ClockTick.hpp"
 
 int frameCtr = 0;
 int gameTime = 0;
@@ -22,59 +22,77 @@ int clientCtr = 0;
 PhysicalObjectManager* manager;
 boost::asio::io_context io_context;
 
-
 // Initializes the frame the server will send back to all clients
-void initializeServerFrame(PhysicalObjectManager* manager, cse125framing::ServerFrame* frame) {
+void initializeServerFrame(PhysicalObjectManager* manager,
+                           cse125framing::ServerFrame* frame)
+{
     frame->ctr = frameCtr++;
     frame->gameTime = gameTime++;
 
-    for (int id = 0; id < cse125constants::NUM_PLAYERS; id++) {
-        ObjPlayer* player = (ObjPlayer*) manager->objects->at(id);
+    for (int id = 0; id < cse125constants::NUM_PLAYERS; id++)
+    {
+        ObjPlayer* player = (ObjPlayer*)manager->objects->at(id);
         frame->players[id].hasCrown = player->hasCrown;
         frame->players[id].makeupLevel = player->makeupLevel;
         frame->players[id].playerDirection = player->direction;
         frame->players[id].playerPosition = vec4(player->position, 1.0f);
         frame->players[id].score = player->score;
-    }    
+    }
 }
 
 // Initializes and returns the PhysicalObjectManager
-PhysicalObjectManager* initializeGame() {
+PhysicalObjectManager* initializeGame()
+{
     PhysicalObjectManager* manager = new PhysicalObjectManager();
     manager->startGame();
     return manager;
 }
 
-void gameLoop(PhysicalObjectManager* manager, int playerId, GameAction gameAction, vec3 cameraDirection) {
-    ObjPlayer* player = (ObjPlayer*) manager->objects->at(playerId);
-    switch (gameAction) {
+void gameLoop(PhysicalObjectManager* manager,
+              int playerId,
+              GameAction gameAction,
+              vec3 cameraDirection)
+{
+    ObjPlayer* player = (ObjPlayer*)manager->objects->at(playerId);
+    switch (gameAction)
+    {
 
     // Basic directions
     case GameAction::MOVE_RIGHT:
-        player->action(glm::normalize(vec3(-cameraDirection.z, cameraDirection.y, cameraDirection.x)));
+        player->action(glm::normalize(
+            vec3(-cameraDirection.z, cameraDirection.y, cameraDirection.x)));
         break;
     case GameAction::MOVE_FORWARD:
         player->action(glm::normalize(cameraDirection));
         break;
     case GameAction::MOVE_LEFT:
-        player->action(glm::normalize(vec3(cameraDirection.z, cameraDirection.y, -cameraDirection.x)));
+        player->action(glm::normalize(
+            vec3(cameraDirection.z, cameraDirection.y, -cameraDirection.x)));
         break;
     case GameAction::MOVE_BACKWARD:
         player->action(glm::normalize(-cameraDirection));
         break;
-    
+
     // Compound directions
     case GameAction::MOVE_FORWARD_RIGHT:
-        player->action(glm::normalize(vec3(-cameraDirection.z, cameraDirection.y, cameraDirection.x) + cameraDirection));
+        player->action(glm::normalize(
+            vec3(-cameraDirection.z, cameraDirection.y, cameraDirection.x) +
+            cameraDirection));
         break;
     case GameAction::MOVE_FORWARD_LEFT:
-        player->action(glm::normalize(vec3(cameraDirection.z, cameraDirection.y, -cameraDirection.x) + cameraDirection));
+        player->action(glm::normalize(
+            vec3(cameraDirection.z, cameraDirection.y, -cameraDirection.x) +
+            cameraDirection));
         break;
     case GameAction::MOVE_BACKWARD_LEFT:
-        player->action(glm::normalize(vec3(cameraDirection.z, cameraDirection.y, -cameraDirection.x) - cameraDirection));
+        player->action(glm::normalize(
+            vec3(cameraDirection.z, cameraDirection.y, -cameraDirection.x) -
+            cameraDirection));
         break;
     case GameAction::MOVE_BACKWARD_RIGHT:
-        player->action(glm::normalize(vec3(-cameraDirection.z, cameraDirection.y, cameraDirection.x) - cameraDirection));
+        player->action(glm::normalize(
+            vec3(-cameraDirection.z, cameraDirection.y, cameraDirection.x) -
+            cameraDirection));
         break;
 
     // Other game actions
@@ -86,9 +104,9 @@ void gameLoop(PhysicalObjectManager* manager, int playerId, GameAction gameActio
     }
 }
 
-void launchServer() {
-
-    std::cout << "Before io_context.run()" << std::endl;
+void launchServer()
+{
+    std::cout << "Launching Server..." << std::endl;
     io_context.run();
 }
 
@@ -109,10 +127,17 @@ int main()
     cse125clocktick::ClockTick ticker(cse125config::TICK_RATE);
 
     // Block until 4 clients connected
-    std::cerr << "Waiting for " << cse125constants::NUM_PLAYERS << " clients to connect..." << std::endl;
-    while (server.clientsConnected < cse125constants::NUM_PLAYERS);
-    std::cerr << cse125constants::NUM_PLAYERS << " clients connected!" << std::endl;
+    std::cerr << "Waiting for " << cse125constants::NUM_PLAYERS
+              << " clients to connect..." << std::endl;
 
+    while (server.clientsConnected < cse125constants::NUM_PLAYERS)
+    {
+        // idle wait for clients
+    }
+
+    std::cerr << server.clientsConnected << " clients connected!" << std::endl;
+
+    // server loop
     try
     {
         while (true)
@@ -120,74 +145,86 @@ int main()
             // Start the clock tick
             ticker.tickStart();
 
-            std::deque<cse125framing::ClientFrame> serverQueue(server.serverQueue);
+            const std::deque<cse125framing::ClientFrame> serverQueue(
+                server.serverQueue);
 
             // Track action information for each player
-            cse125gameaction::GameActionTracker gameActionTracker(cse125constants::NUM_PLAYERS);
-            std::vector<int> playerPriorities(cse125constants::NUM_PLAYERS, 0); // index i stores player i's priority
+            cse125gameaction::GameActionTracker gameActionTracker(
+                cse125constants::NUM_PLAYERS);
+
+            // index i stores player i's priority
+            std::vector<int> playerPriorities(cse125constants::NUM_PLAYERS, 0);
+
             unsigned int priorityCtr = 1;
 
-            // Iterate from the end of where the queue currently is to the beginning
-            for (auto it = serverQueue.crbegin(); it != serverQueue.crend(); it++)
+            // Iterate from the end of where the queue currently is to the
+            // beginning
+            for (auto it = serverQueue.crbegin(); it != serverQueue.crend();
+                 it++)
             {
                 const cse125framing::ClientFrame& clientFrame = *it;
-                // Accumulate this player's action
-                // std::cerr << "Client id is " << clientFrame.id << std::endl;
 
-                gameActionTracker.setAction(clientFrame.id, clientFrame.movementKey, clientFrame.cameraDirection);
+                gameActionTracker.setAction(clientFrame.id,
+                                            clientFrame.movementKey,
+                                            clientFrame.cameraDirection);
+
                 // Track the priority order for this player
-                if (!playerPriorities.at(clientFrame.id)) {
+                if (!playerPriorities.at(clientFrame.id))
+                {
                     playerPriorities.at(clientFrame.id) = priorityCtr++;
-                }                
+                }
             }
 
-            size_t sizeBefore = serverQueue.size();
-            // std::cerr << "Queue size before: " << serverQueue.size() << std::endl;
             // Empty the queue of all tasks
             server.queueMtx.lock();
             server.serverQueue.clear();
             server.queueMtx.unlock();
-            // std::cerr << "Queue size after: " << serverQueue.size() << std::endl;
-
 
             // Determine the sorted priority order
-            std::vector<std::pair<int, int>> sortedPriorities; // <client id, priority> pairs
-            for (size_t i = 0; i < playerPriorities.size(); i++) {
-                sortedPriorities.push_back(std::make_pair(i, playerPriorities.at(i)));
+            // <client id, priority> pairs
+            std::vector<std::pair<int, int>> sortedPriorities;
+            for (size_t i = 0; i < playerPriorities.size(); i++)
+            {
+                sortedPriorities.push_back(
+                    std::make_pair(i, playerPriorities.at(i)));
             }
             // Note: Lower values indicate higher priority
-            std::sort(sortedPriorities.begin(), sortedPriorities.end(), [](auto& left, auto& right) {return left.second < right.second; });
-
+            std::sort(sortedPriorities.begin(), sortedPriorities.end(),
+                      [](auto& left, auto& right)
+                      { return left.second < right.second; });
 
             // Update basic game state (not dependent on input)
             manager->step();
             // Update the game state in player priority order
-            for (auto it = sortedPriorities.begin(); it != sortedPriorities.end(); it++) {
-                const int& playerId = it->first;                
-                const cse125gameaction::GameActionContainer* container = gameActionTracker.getGameActionContainer(playerId);
-                GameAction gameAction = cse125gameaction::gameActionFromContainer(container);
-                gameLoop(manager, playerId, gameAction, container->cameraDirection);
+            for (auto it = sortedPriorities.begin();
+                 it != sortedPriorities.end(); it++)
+            {
+                const int& playerId = it->first;
+                const cse125gameaction::GameActionContainer* container =
+                    gameActionTracker.getGameActionContainer(playerId);
+                GameAction gameAction =
+                    cse125gameaction::gameActionFromContainer(container);
+                gameLoop(manager, playerId, gameAction,
+                         container->cameraDirection);
             }
 
-            // TODO: Call Game loop even if no packets from any players to update score, etc. (might be a score-specific game loop)
+            // TODO: Call Game loop even if no packets from any players to
+            // update score, etc. (might be a score-specific game loop)
 
             // Write data back to players
             cse125framing::ServerFrame serverFrame;
             initializeServerFrame(manager, &serverFrame);
-            // if (sizeBefore > 0) {
-                server.writePackets(&serverFrame);
-            // }    
+            server.writePackets(&serverFrame);
 
             // Sleep until the end of the clock tick
-            ticker.tickEnd();   
+            ticker.tickEnd();
         }
-    
     }
     catch (std::exception& e)
     {
         std::cerr << e.what() << std::endl;
     }
-    
+
     // Wait for the server thread to finish
     serverThread.join();
 
