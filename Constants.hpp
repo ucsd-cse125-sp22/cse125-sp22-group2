7e807af37b20f96b86e580ee64c778760ae9f2f7
evanjserrano <e1serran@ucsd.edu>
2022-05-19 11:18:32 -0700

    change NUM_MAKEUP_BOOTHS to _STATIONS

--- Constants.hpp
+++ Constants.hpp
@@ -5,6 +5,6 @@ namespace cse125constants {
 	const int DEFAULT_MAKEUP_LEVEL = 100;
 	const int DEFAULT_SCORE = 0;
 	const int MAX_NUM_SOUNDS = 4;
-	const int NUM_MAKEUP_BOOTHS = 2;
+	const int NUM_MAKEUP_STATIONS = 2;
 	const int NUM_PLAYERS = 4;
 }
