655d287a4e3312a0830f838811f9720bdbe9300e
evanjserrano <e1serran@ucsd.edu>
2022-05-25 13:36:12 -0700

    add game time to config

--- Config.hpp
+++ Config.hpp
@@ -4,9 +4,11 @@ namespace cse125config {
 	const std::string DEFAULT_SERVER_HOST = "localhost";
 	const std::string DEFAULT_SERVER_PORT = "8000";
 	const unsigned int DEFAULT_TICK_RATE = 30;
+	const float DEFAULT_MATCH_LENGTH = 30.0f;
 	extern std::string SERVER_HOST;
 	extern std::string SERVER_PORT;
 	extern int TICK_RATE;
+	extern float MATCH_LENGTH;
 
 	/*
 	 * @brief Initializes all of the variables in the configuration namespace using the
